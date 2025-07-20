757ef26451508106fb60297920afb670a4d787cd
Alvin McClintock <alvin.mcclintock@carters.com>
2025-07-20 15:16:54 -0400

    lots of changes up to 47.24 in video 1

---
+++ app/final/management/commands/wait_for_db.py
@@ -0,0 +1,19 @@
+import time
+
+from psycopg2 import OperationalError as Psycopg2OpError
+
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    def handle(self,*args,**options):
+        self.stdout.write("Waiting for databse...")
+        db_up = False
+        while not db_up:
+            try:
+                self.check(databases=['defaults'])
+                db_up=True
+            except (Psycopg2OpError,OperationalError):
+                self.stdout.write("Databse unavailable, waiting 1 second.....")
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS('Database available!'))
---
+++ app/final/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import redirect,render,get_object_or_404
+from django.views.generic import View
+from django.template import loader
+from django.http import HttpResponse
+#from .models import Book,Branch,Inventory
+
+class HomePageView(View):
+    def get(self,request):
+        
+        template  = loader.get_template('final/home.html')
+
+        context = {
+            'title':'Henry Bookstore'
+        }
+        return HttpResponse(template.render(context,request))
